86c0651461d611d26a0740a7edfdee6c522bd926
swapnilkha <swapnilkha@gmail.com>
2018-05-30 22:24:12 -0400

    Update AdvancedCheckersTester.java

--- AdvancedCheckersTester.java
+++ AdvancedCheckersTester.java
@@ -3,7 +3,6 @@ but in order to win either all of the opponent's pieces must be gone, OR, three 
 opponent's last row. This is the tester class, and the game class is in the other file. */
 
 
-
 import java.io.*;
 import java.util.*;
 
@@ -13,18 +12,23 @@ public class AdvancedCheckersTester
     public static void main(String[] args) throws IOException
     {
         //initialize class object, print out directions of the game and if game is over display the winner
+        
         AdvancedCheckers AC = new AdvancedCheckers();
         System.out.println("Welcome to AdvancedCheckers. To win the game, eliminate all pieces of your opponent or get three of your pieces to the other side.");
         System.out.println("Big red and Big Black can move one step in all four directions");
         System.out.println("All other pieces can move one step forward diagonally");
         AC.display();
+        
         //display the board and run the game
+        
         while(!AC.gameIsOver())
         {
             AC.move();
             AC.display();
         }
+        
         //at the end, print the winner
+        
         System.out.println(AC.whoWon() + " won the game");
     }
 }
